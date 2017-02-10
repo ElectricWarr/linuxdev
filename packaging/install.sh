d3ef690958e1b369a631df15a267b2ca81121205
Michael Warr <mtw_w@hotmail.com>
2017-02-10 14:45:31 +0000

    initial commit

---
+++ packaging/install.sh
@@ -0,0 +1,36 @@
+# --------------------------
+# Script to install linuxdev
+# Author: Michael Warr
+# Version: 0.0.0
+# --------------------------
+
+# Functions
+datetimestamp () {
+  # Only down to the second :(
+  date +%Y%m%d%H%M%S
+}
+
+backup_file () {
+  local source="$1"
+  mkdir -p "$backup_dir"
+  cp "$source" "$backup_dir"
+}
+
+move_and_backup () {
+  local source="$1"
+  local target="$2"
+  [[ -f "$target" ]] && backup_file "$target"
+  mv -f "$source" "$target"
+}
+
+# Variables
+linuxdev_dir="$HOME/.linuxdev"
+backup_dir="$linuxdev_dir"/backup_$(datetimestamp)
+
+# Create .linuxdev
+mkdir -p "$linuxdev_dir"
+
+# Deploy, backuping up any files as necessary
+move_and_backup 'Dockerfile' "$linuxdev_dir"'/Dockerfile'
+move_and_backup 'docker-compose.yml' "$linuxdev_dir"'/docker-compose.yml'
+move_and_backup 'linuxdev' "$linuxdev_dir"'/linuxdev'
---
+++ packaging/package.sh
@@ -0,0 +1,34 @@
+# --------------------------
+# Script to package linuxdev
+# Author: Michael Warr
+# Version: 0.1.0
+# Requirements:
+#   makeself
+# --------------------------
+
+usage () {
+  printf 'usage: package.sh RELEASE\n'
+  printf 'RELEASE   Version of current release, like 1.1.1\n'
+  printf '\n'
+}
+
+if [[ $# -ne 1 ]] || [[ ! "$1" =~ [0-9]\.[0-9]\.[0-9] ]]; then
+  usage
+  exit 1
+fi
+
+release="$1"
+
+# Prepare release in a temporary directory
+temp_dir="$(mktemp -d)"
+release_dir="$temp_dir"'/'"$release"
+
+mkdir "$release_dir"
+cp ../source/* "$release_dir"
+cp 'install.sh' "$release_dir"
+
+# Package using makeself
+makeself "$release_dir" '../releases/linuxdev_'"$release"'.run' 'linuxdev v'"$release" './install.sh'
+
+# Cleanup
+rm -rf "$temp_dir"
